620e2203c8009c3f884f47b8afed5e1a4f0bee59
Inaho <Inahocyc@gmail.com>
2022-01-29 22:44:36 +0800

    use opaque error instead

--- internal/biz/publish_record.go
+++ internal/biz/publish_record.go
@@ -1,28 +1,23 @@
 package biz
 
 import (
-	"database/sql"
 	"errors"
 
 	"github.com/cycade/chaos-errors/internal/data"
-	pkgErrors "github.com/pkg/errors"
 )
 
 const MOCK_ARTIFACT = "path/to/python3"
 
-// 寻找 AppId 所对应的产物路径，在允许使用兜底的情况下使用 MOCK_ARTIFACT
+// 寻找 AppId 所对应的产物路径，在 允许使用兜底(allowMock) 的情况下返回 MOCK_ARTIFACT
 func GetArtifactPath(appId string, allowMock bool) (string, error) {
 	record, err := data.FindPublishRecordByAppId(appId)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			if allowMock {
-				return MOCK_ARTIFACT, nil
-			} else {
-				return "", pkgErrors.Wrap(err, "未能找到 AppId 现存的产物路径")
-			}
+		var sqlError data.SQLError
+		if errors.As(err, &sqlError) && sqlError.IsDataNotFound() && allowMock {
+			return MOCK_ARTIFACT, nil
 		}
 
-		return "", pkgErrors.Wrap(err, "未能找到 AppId 现存的产物路径")
+		return "", err
 	}
 
 	return record.ArtifactPath, nil
--- internal/data/pubulish_record.go
+++ internal/data/pubulish_record.go
@@ -1,6 +1,10 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+
+	pkgErrors "github.com/pkg/errors"
+)
 
 type PublishRecord struct {
 	Id           string
@@ -8,11 +12,27 @@ type PublishRecord struct {
 	ArtifactPath string
 }
 
-func FindPublishRecordByAppId(id string) (PublishRecord, error) {
+func FindPublishRecordByAppId(appId string) (PublishRecord, error) {
 	var record PublishRecord
 	// err := db.QueryRow("select id, app_id, artifact_path from publish_record where app_id = ?", id).Scan(&record)
 
 	// 在此处并没有办法处理这个错误，甚至都不知道 ErrNoRows 对于业务逻辑来说是不是一个错误
-	// 由业务层在发现真正业务逻辑错误时，进行 wrap 获取堆栈信息，在此处 wrap 会导致堆栈信息冗余
-	return record, sql.ErrNoRows
+	err := ChaosError{err: sql.ErrNoRows}
+	return record, pkgErrors.Wrap(err, "未能找到 AppId 的产物路径")
+}
+
+type SQLError interface {
+	IsDataNotFound() bool
+}
+
+type ChaosError struct {
+	err error
+}
+
+func (c ChaosError) Error() string {
+	return c.err.Error()
+}
+
+func (c ChaosError) IsDataNotFound() bool {
+	return pkgErrors.Is(c.err, sql.ErrNoRows)
 }
